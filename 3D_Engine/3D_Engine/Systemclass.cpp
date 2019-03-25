cfdac32232c5c721b44342bb1922ee2b9ad4e7b8
Alexandre Paquet <paquet.alex@gmail.com>
2019-03-25 19:38:40 +0900

    Renaming of application

--- 3D_Engine/3D_Engine/Systemclass.cpp
+++ 3D_Engine/3D_Engine/Systemclass.cpp
@@ -274,7 +274,7 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 	m_hinstance = GetModuleHandle(NULL);
 
 	// Give the application a name.
-	m_applicationName = L"Engine";
+	m_applicationName = L"3D Engine";
 
 	// Setup the windows class with default settings.
 	wc.style         = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
